cd7ba3fcbdbfd87a5717480e9a983c842b7b358d
ethanscho <>
2020-09-02 21:49:59 +0900

    Add Lu Zhang strategy

--- quantitative-value/models/lu_zhang.py
+++ quantitative-value/models/lu_zhang.py
@@ -11,15 +11,15 @@ def get_stocks(filepath, date, verbose=True):
         print(date, filepath)
     df = pd.read_csv(filepath, dtype={"기업코드":"string", "종목코드":"string"})
     
-#     past_income_cols = []
-#     last_year = int(date[:4]) - 1
-#     for i in range(1, 6):
-#         year = last_year - i
-#         col_name = str(year) + '-' + '당기순이익'
-#         past_income_cols.append(col_name)
+    past_asset_cols = []
+    last_year = int(date[:4]) - 1
+    for i in range(1, 6):
+        year = last_year - i
+        col_name = str(year) + '-' + '자본총계'
+        past_asset_cols.append(col_name)
         
-    cols = ['종목코드', 'IFRS', 'CFS', '회사명', '시가총액', 'PBR', 'GP/A', '당기순이익', '증자', '영업활동으로인한현금흐름', '유동비율', '주당배당금']
-    #cols.extend(past_income_cols)
+    cols = ['종목코드', 'IFRS', 'CFS', '회사명', '시가총액', 'ROE', '자본총계']
+    cols.extend(past_asset_cols)
 
     df = df[cols]
     if verbose:
@@ -35,38 +35,18 @@ def get_stocks(filepath, date, verbose=True):
     if verbose:
         print('국외주식 제외', len(df))
     
-#     # 유동비율 > 100%
-#     df = df[df['유동비율'] > 1.0]
-#     if verbose:
-#         print('유동비율 > 1', len(df))
-        
-#     # 주당배당금 > 0
-#     df = df[df['주당배당금'] > 0.0]
+    # 자산성장률
+    df['자본성장률'] = (df['자본총계'] - df[past_asset_cols[0]]) / df[past_asset_cols[0]]
 
-#     # 전년도 증자 기업 제외
-#     df_qp = df_qp[df_qp['증자'] == 0]
-#     print('증자기업 제외', len(df_qp))
-
-#     # 전년도 적자 기업 제외 
-#     df_qp = df_qp[df_qp['당기순이익'] > 0]
-#     print('당기순이익 적자 제외', len(df_qp))
-    
-#     # 전년도 영업현금흐름 적자 기업 제외 
-#     df_qp = df_qp[df_qp['영업활동으로인한현금흐름'] > 0]
-#     print('영업활동으로인한현금흐름 적자 제외', len(df_qp))
-    
-    # 당기순이익 최근 5년 +인것만
-    #df_qp = exclude_minus_income_corps(df_qp, past_income_cols)
-
-    # 시가총액 하위 20% 
+    # 시가총액
     df = df[df['시가총액'] > 0] # 시가총액 data가 없는 row는 제거
     df = df.sort_values(by=['시가총액'])
     df = df[int(len(df)*MIN_MARKET_CAP):int(len(df)*MAX_MARKET_CAP)]
     
     # get ranks
-    df['1/PBR'] = 1 / df['PBR']
-    df['RANK_1'] = df['1/PBR'].rank(ascending=False)
-    df['RANK_2'] = df['GP/A'].rank(ascending=False) # 높을 수록 좋음
+    df['1/자본성장률'] = 1 / df['자본성장률']
+    df['RANK_1'] = df['1/자본성장률'].rank(ascending=False)
+    df['RANK_2'] = df['ROE'].rank(ascending=False)
     
     df['TOTAL_RANK'] = df['RANK_1'] + df['RANK_2']
 
